a7ab33a0d57d5086fcdd7aee378a5b6001d36508
FMB555 <49597989+FMB555@users.noreply.github.com>
2020-10-03 17:07:46 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Home from './Componentes/Home'
+import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path='/' component={Home} exact />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App;
+
+
+
+  /* */
---
+++ src/Componentes/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Chart from '../Partes/Chart'
+
+export default function Home(){
+    return (
+        <div>
+            <Chart></Chart>
+        </div>
+    )   
+}
