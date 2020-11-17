e08288f684a5c2a30ea17ee99e6910bc8c33ccd4
Juan Pablo Pieczocha <70308282+JPPieczocha@users.noreply.github.com>
2020-11-17 16:38:42 -0300

    Carga campo arreglado

--- src/Components/ModalCargaCampo.js
+++ src/Components/ModalCargaCampo.js
@@ -1,85 +1,54 @@
-import React, { Component } from 'react'
-
-import ListLotes from './ListLotes'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
 
 import { Grid, TextField, Button, Divider } from '@material-ui/core'
 
-export default class ModalCargaCampo extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            nombreCampo: '',
-            cantHectareas: '', 
-            lotes: [],
-        }
-        
-        this.addLote = this.addLote.bind(this)
-        this.handleNombre = this.handleNombre.bind(this)
-        this.handleHectareas = this.handleHectareas.bind(this)
-        this.deleteLote = this.deleteLote.bind(this)
+export default function ModalCargaCampo() {
+
+    const [nombre, setNombre] = useState("")
+    const handleNombre = (e) => {
+        setNombre(e.target.value)
     }
 
-    handleNombre(e) {
-        this.setState ({
-            nombreCampo: e.target.value
-        })
+    const [hectareas, setHectareas] = useState("")
+    const handleHectareas = (e) => {
+        setHectareas(e.target.value)
     }
-
-    handleHectareas(e){
-        this.setState ({
-            cantHectareas: e.target.value
-        })
+    
+    const [file, setFile] = useState(null)
+    const handleFile = (e) => {
         console.log(e.target.value)
     }
 
-    addLote(e) {
-        e.preventDefault()
-        const lote = Date.now()
-        this.setState({
-            lotes: [...this.state.lotes, lote]
-        })
-    }
-
-    deleteLote(key) {
-        const lotesFilt = this.state.lotes.filter( lote => lote !== key)
-        this.setState({
-            lotes: lotesFilt
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                <form>
-                    <h1>Cargá tu campo</h1>
-
-                    {/*Cargar datos generales del campo*/}
-                    <Grid container spacing={3}>
-                        <Grid item xs={12} sm={8}>
-                            <TextField id="outlined-basic" value={this.state.nombreCampo} label="Ingrese el Nombre de su Campo" variant="outlined" fullWidth onChange={this.handleNombre} required/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <TextField id="outlined-basic" label="Ingresar la Cantidad de Hectáreas" variant="outlined" type='number' fullWidth onChange={this.handleHectareas} required/>
-                        </Grid>
+    return (
+        <div>
+            <form>
+                <h1>Cargá tu campo</h1>
+                {/*Cargar datos generales del campo*/}
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={8}>
+                        <TextField id="outlined-basic" label="Ingrese el Nombre de su Campo" variant="outlined" fullWidth={true} onChange={handleNombre} required/>
                     </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <TextField id="outlined-basic" label="Ingresar la Cantidad de Hectáreas" variant="outlined" type='number' fullWidth={true} onChange={handleHectareas} required/>
+                    </Grid>
+                </Grid>
+                <br/>
 
-                    <Button onClick={this.addLote}>Crear Lote</Button>
+                <Divider />
+                <h3>Archivo</h3>
+                <p>Deje su arhivo aquí, por favor ingrese únicamente la plantilla provista por nosotros y recuerde completar las hojas con los datos que correspondan.</p>
+                <input type='file' required/>
+                <br/>
+                <br/>
+                <br/>
 
-                    <ListLotes lotes={this.state.lotes} deleteLote={this.deleteLote}/>
+                <Button type='submit' variant="contained" color="primary"> Finalizar </Button>
+                <br/><br/>
+                <p>¿No tiene nuestra plantilla? Puede descargarla haciendo click <Link to='/'>acá</Link>.</p>
 
-                    <Divider />
+            </form>
 
-                    <h3>Empleados</h3>
-                    <p>Cargue sus empleados aquí</p>
-                    <input type='file' required/>
-                    <Divider />
-
-                    <Button type='submit'> Finalizar </Button>
-
-                </form>
-
-            </div>
-        )
-
-    }
+        </div>
+    )
 }
